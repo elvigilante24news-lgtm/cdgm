15c2dd2879456542583247a93cd3b4631c004455
jony <jonyxdxp@gmail.com>
2026-05-29 14:44:07 -0300

    Remove repository contents (empty repo)

--- db/relations.ts
+++
@@ -1,4 +0,0 @@
-export {
-  matriculadosRelations,
-  notificationsRelations,
-} from "./schema";
--- db/reset.sql
+++
@@ -1,5 +0,0 @@
-
-DROP TABLE IF EXISTS site_content;
-DROP TABLE IF EXISTS system_config;
-DROP TABLE IF EXISTS notifications;
-DROP TABLE IF EXISTS matriculados;
